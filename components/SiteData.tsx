11526f57ba3c02588e75dbeda8320f726f70f07a
malweis <58945171+malweis@users.noreply.github.com>
2023-06-25 17:19:48 -0400

    feat: Añade carrusel de imagenes

--- components/SiteData.tsx
+++ components/SiteData.tsx
@@ -1,67 +1,32 @@
-import React from "react"
-
-import { Imagenes } from "./Imagenes"
+"use client"
+import React, {useEffect, useState, useRef} from "react";
+import svgs from "./Images";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const SiteData = () => {
+  const [width, setWidth] = useState(0);
+  const carrousel = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (carrousel.current) { 
+      setWidth(carrousel.current.scrollWidth  - carrousel.current.offsetWidth)
+    }
+  }, [])
+  
+
   return (
-    <div className=" slider w-full">
-      <div className="slidetrack">
-        <div className="slide">
-          <Imagenes
-            source="/iconos/react-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-        <div className="slide">
-          {" "}
-          <Imagenes
-            source="/iconos/vite-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-        <div className="slide">
-          {" "}
-          <Imagenes
-            source="/iconos/next-dot-js-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-        <div className="slide">
-          {" "}
-          <Imagenes
-            source="/iconos/tailwind-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-        <div className="slide">
-          {" "}
-          <Imagenes
-            source="/iconos/java-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-        <div className="slide">
-          {" "}
-          <Imagenes
-            source="/iconos/spring-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-        <div className="slide">
-          {" "}
-          <Imagenes
-            source="/iconos/postgresql-svgrepo-com.svg"
-            width={40}
-            height={40}
-          />
-        </div>
-      </div>
-    </div>
-  )
-}
+    <motion.div ref={carrousel} className=" w-full  cursor-grab overflow-hidden">
+      <motion.div 
+      drag="x"
+      dragConstraints={{ left: -width, right: 0 }}
+      className="flex justify-evenly w-full gap-4">
+        {svgs.map((svg, index) => (
+          <motion.div className="h-28 w-[200px] p-2 " key={index}>
+            <Image src={svg} alt="tecnologias conocidas" className="w-full p-2 h-full pointer-events-none imagenes" />
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
